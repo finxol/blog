9f70bfc38bd335e0d50cd2c50724ea0607a2b384
finxol <git@finxol.io>
2024-11-14 21:59:36 +0100

    fix: grow post content when too small for viewport

---
+++ content/posts/blog-rewrite.md
@@ -0,0 +1,12 @@
+---
+title: I rewrote my blog and it was harder than I expected
+description: The last time I changed anything on my previous blog was almost exactly 2 years ago. Wayyy too long. So I rewrote it completely.
+date: 2024-11-14
+authors:
+  - name: finxol
+tags:
+  - code
+  - rants
+---
+
+I never was fully satisfied by
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -2,58 +2,62 @@
 </script>
 
 <template>
-    <button
-        class="flex items-center gap-1 text-sm text-neutral-600 hover:text-neutral-900 mt-4"
-        @click="$router.back()"
+    <div
+        class="grow"
     >
-        <Icon name="tabler:chevron-left" />
-        Go back
-    </button>
-    <ContentQuery :path="$route.path" find="one">
-        <template #default="{ data }">
-            <main class="pb-12">
-                <header class="mb-8 mt-4">
-                    <h1 class="text-4xl font-bold">
-                        {{ data?.title }}
-                    </h1>
-                    <div class="flex items-center justify-start gap-4 mt-4">
-                        <div v-if="data.authors" class="flex items-center gap-1">
-                            <img v-if="data.authors[0].name === 'finxol'" src="/logo.png" :alt="data.authors[0].name"
-                                class="w-8 h-8 rounded-full mr-2">
-                            <span v-for="author in data.authors" :key="author.name" class="text-neutral-600">
-                                {{ author.name }}
+        <button
+            class="flex items-center gap-1 text-sm text-neutral-600 hover:text-neutral-900 mt-4"
+            @click="$router.back()"
+        >
+            <Icon name="tabler:chevron-left" />
+            Go back
+        </button>
+        <ContentQuery :path="$route.path" find="one">
+            <template #default="{ data }">
+                <main class="pb-12">
+                    <header class="mb-8 mt-4">
+                        <h1 class="text-4xl font-bold">
+                            {{ data?.title }}
+                        </h1>
+                        <div class="flex items-center justify-start gap-4 mt-4">
+                            <div v-if="data.authors" class="flex items-center gap-1">
+                                <img v-if="data.authors[0].name === 'finxol'" src="/logo.png" :alt="data.authors[0].name"
+                                    class="w-8 h-8 rounded-full mr-2">
+                                <span v-for="author in data.authors" :key="author.name" class="text-neutral-600">
+                                    {{ author.name }}
+                                </span>
+                            </div>
+                            ·
+                            <span class="text-neutral-600">
+                                {{ new Date(data.date).toLocaleDateString('en-GB', {
+                                    year: 'numeric',
+                                    month: 'long',
+                                    day: 'numeric'
+                                }) }}
                             </span>
                         </div>
-                        ·
-                        <span class="text-neutral-600">
-                            {{ new Date(data.date).toLocaleDateString('en-GB', {
-                                year: 'numeric',
-                                month: 'long',
-                                day: 'numeric'
-                            }) }}
-                        </span>
+
+                        <div class="bg-stone-200 h-[1px] my-4"></div>
+
+                        <p
+                            class="text-md text-neutral-500 leading-7 mb-8"
+                        >
+                            {{ data.description }}
+                        </p>
+                    </header>
+
+                    <div class="bg-stone-200 h-[1px] my-8"></div>
+                    <ContentDoc :path="data._path" class="prose prose-lg prose-slate max-w-none mx-auto" />
+                </main>
+            </template>
+            <template #not-found>
+                <div class="flex items-center justify-center">
+                    <div class="text-center">
+                        <h1 class="text-4xl font-bold">404</h1>
+                        <p class="text-neutral-500">Page not found</p>
                     </div>
-
-                    <div class="bg-stone-200 h-[1px] my-4"></div>
-
-                    <p
-                        class="text-md text-neutral-500 leading-7 mb-8"
-                    >
-                        {{ data.description }}
-                    </p>
-                </header>
-
-                <div class="bg-stone-200 h-[1px] my-8"></div>
-                <ContentDoc :path="data._path" class="prose prose-lg prose-slate max-w-none mx-auto" />
-            </main>
-        </template>
-        <template #not-found>
-            <div class="flex items-center justify-center">
-                <div class="text-center">
-                    <h1 class="text-4xl font-bold">404</h1>
-                    <p class="text-neutral-500">Page not found</p>
                 </div>
-            </div>
-        </template>
-    </ContentQuery>
+            </template>
+        </ContentQuery>
+    </div>
 </template>
