fba4323cc074685adf3a4ce77a6b485defd343a3
finxol <git@finxol.io>
2025-03-31 06:36:34 +0200

    feat: new blog post

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,15 +6,15 @@ export default defineNuxtConfig({
         "@nuxt/content",
         "@nuxtjs/tailwindcss",
         "@tailwindcss/typography",
-        "@nuxt/icon"
+        "@nuxt/icon",
     ],
 
     app: {
         head: {
             htmlAttrs: {
-                lang: "en"
-            }
-        }
+                lang: "en",
+            },
+        },
     },
 
     icon: {
@@ -25,19 +25,19 @@ export default defineNuxtConfig({
                 "ri:mastodon-fill",
                 "ri:bluesky-fill",
                 "ri:sun-fill",
-                "ri:moon-fill"
+                "ri:moon-fill",
             ],
-            scan: true
-        }
+            scan: true,
+        },
     },
 
     typescript: {
-        typeCheck: true
+        typeCheck: true,
     },
 
     routeRules: {
-        "/": { prerender: true }
+        "/": { prerender: true },
     },
 
-    compatibilityDate: "2025-03-17"
+    compatibilityDate: "2025-03-17",
 });
