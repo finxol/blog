37eaf6f49ea229c9c124f0fc73b6af03a6a4aad7
finxol <git@finxol.io>
2025-09-09 10:12:17 +0200

    feat(ui): add Bookmark component

---
+++ app/components/content/Bookmark.vue
@@ -0,0 +1,26 @@
+<script setup>
+const props = defineProps({
+    url: String
+});
+</script>
+
+<template>
+    <NuxtLink
+        :to="url"
+        :class="[
+            'flex flex-row items-center justify-start gap-4',
+            'px-2 py-4 md:px-4 md:py-6 not-prose',
+            'border border-gray-300 dark:border-gray-700 border-solid border-2 rounded-lg',
+            'bg-stone-200/25 hover:bg-stone-200/50 dark:bg-stone-700/25 dark:hover:bg-stone-700/50',
+            'transition-colors duration-200'
+        ]"
+    >
+        <Icon name="ri:bookmark-fill" size="1.5rem" class="text-yellow-700" />
+        <div class="flex flex-col items-start justify-start">
+            <slot />
+            <span class="text-xs text-gray-400 dark:text-gray-600">
+                {{props.url}}
+            </span>
+        </div>
+    </NuxtLink>
+</template>
