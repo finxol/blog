15a79e9207faedb681a63d191f553b67a4ad5e82
finxol <git@finxol.io>
2025-09-09 19:03:47 +0200

    fix: improve print and mobile styles

--- app/components/content/Bookmark.vue
+++ app/components/content/Bookmark.vue
@@ -9,14 +9,15 @@ const props = defineProps({
         :to="url"
         :class="[
             'flex flex-row items-center justify-start gap-4',
-            'px-2 py-4 md:px-4 md:py-6 not-prose',
+            'px-4 py-4 md:px-6 md:py-6 not-prose',
             'border border-gray-300 dark:border-gray-700 border-solid border-2 rounded-lg',
             'bg-stone-200/25 hover:bg-stone-200/50 dark:bg-stone-700/25 dark:hover:bg-stone-700/50',
-            'transition-colors duration-200'
+            'transition-colors duration-200',
+            'bookmark-link'
         ]"
     >
         <Icon name="ri:bookmark-fill" size="1.5rem" class="text-yellow-700" />
-        <div class="flex flex-col items-start justify-start">
+        <div class="bookmark-content flex flex-col items-start justify-start font-bold">
             <slot />
             <span class="text-xs text-gray-400 dark:text-gray-600">
                 {{props.url}}
@@ -24,3 +25,12 @@ const props = defineProps({
         </div>
     </NuxtLink>
 </template>
+
+<style scope>
+@media print {
+    a.bookmark-link::after {
+        content: "";
+        display: none;
+    }
+}
+</style>
